c14ac3db05dd1320d03f57fc743ffcc081c08100
54mur41mx <c.albert15va@gmail.com>
2023-01-13 16:56:40 -0600

    Hasta aqui me quede xd :'v

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,13 +1,19 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
+let boton = document.getElementById("button");
+let entrada = document.getElementById("input");
+let respuesta = document.getElementById("answer");
 
-/**
- * STEPS:
- *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
- * 4. Clear output after 3 seconds
- * 5. Optional: add loading/error states
- *
- */
+
+
+boton.addEventListener("click", function(){
+    function fetchAnswer(){
+        fetch(API_ENDPOINT)
+        .then(respuesta => respuesta.json())
+        .then(data => (respuesta.innerHTML = data.answer))
+    }
+
+} )
+
+
+
